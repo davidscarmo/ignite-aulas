543d4a5d7e142cbd7f4dbb5d8d2d9d5dd98e59e4
David Carmo <davidsc2812@gmail.com>
2021-07-25 11:06:27 -0300

    Estilizacao da pagina Home

--- chapter3/ignews/src/pages/index.tsx
+++ chapter3/ignews/src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from "next/head";
-
+import styles from "./home.module.scss";
 export default function Home() {
   return (
     <>
       <Head>
         <title> Início | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>there!</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero} >
+          <span>👏 Hey, Welcome!</span>
+          <h1>
+            News About the <span>React</span> world!
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }

2d0a2d6b49008f815bbeb838842d9e2f1b1c7455
David Carmo <davidsc2812@gmail.com>
2021-07-27 06:39:36 -0300

    Criacao da funcionalidade para consumir a API do Stripe

---
+++ chapter3/ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.scss";
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      {" "}
+      Subscribe Now!{" "}
+    </button>
+  );
+}
--- chapter3/ignews/src/pages/index.tsx
+++ chapter3/ignews/src/pages/index.tsx
@@ -1,24 +1,55 @@
+  import { GetStaticProps } from "next";
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title> Início | ig.news</title>
       </Head>
       <main className={styles.contentContainer}>
-        <section className={styles.hero} >
+        <section className={styles.hero}>
           <span>👏 Hey, Welcome!</span>
           <h1>
             News About the <span>React</span> world!
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JH8M5LcoIv6OTSUNYl5Jf4w", {
+    expand: ["product"],
+  }); //price__1JH... it comes from the stripe product -> price
+
+  const product = {
+    price: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100), // the amount come in cents
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60*60*24, //set the time to generate again the page in 24h
+  };
+};

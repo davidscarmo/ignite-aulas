8402a6a31be4891955dbb5ca6397998596767acc
David Carmo <davidsc2812@gmail.com>
2021-08-02 20:10:29 -0300

    Integracao com o OAuth do GitHub utilizando o NextAuth

--- chapter3/ignews/src/components/SingInButton/index.tsx
+++ chapter3/ignews/src/components/SingInButton/index.tsx
@@ -1,16 +1,26 @@
 import styles from "./styles.module.scss";
+import { signIn, signOut, signout, useSession } from "next-auth/client";
 import { FiX } from "react-icons/fi";
 import { FaGithub } from "react-icons/fa";
 export function SingInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const [session] = useSession();
+  console.log(session);
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      David Carmo
+      {session.user.name}
       <FiX color="#737388" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sing in with GitHub
     </button>
---
+++ chapter3/ignews/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: "read:user",// it sets what you will receive from the request // only basics infos about user
+    }),
+    // ...add more providers here
+  ],
+});

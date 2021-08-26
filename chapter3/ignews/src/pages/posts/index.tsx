e4eb7f51837309122224db0a91591425a08b06ef
David Carmo <davidsc2812@gmail.com>
2021-08-26 03:03:22 -0300

    Conectando com a api do prismic

--- chapter3/ignews/src/pages/posts/index.tsx
+++ chapter3/ignews/src/pages/posts/index.tsx
@@ -1,6 +1,8 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
-
+import Prismic from "@prismicio/client";
 export default function Posts() {
   return (
     <>
@@ -39,3 +41,19 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+  console.log(response);
+  return {
+    props: {},
+  };
+};

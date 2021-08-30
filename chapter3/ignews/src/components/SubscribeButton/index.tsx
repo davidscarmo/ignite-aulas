fcdf99b538d96f67338cd2a6ae0941571eb84bfc
David Carmo <davidsc2812@gmail.com>
2021-08-30 05:18:47 -0300

    Validacao da assinatura ativa

--- chapter3/ignews/src/components/SubscribeButton/index.tsx
+++ chapter3/ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 import { api } from "../../services/api";
 import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
@@ -9,13 +10,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
-
+  const router = useRouter();
   async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
     }
-
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
     try {
       const response = await api.post("/subscribe"); //acess the route who creates the checkoutSession
       const { sessionId } = response.data; // gets the id of the checkoutSession
--- chapter3/ignews/src/pages/api/auth/[...nextauth].ts
+++ chapter3/ignews/src/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,33 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+        console.log("ué");
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch {
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
       try {

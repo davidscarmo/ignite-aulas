f7629fe4c2bd7a97c40736087d1b546079cfb378
David Carmo <davidsc2812@gmail.com>
2021-08-30 06:07:58 -0300

    Criacao da pagina de preview dos posts

--- chapter3/ignews/src/pages/posts/[slug].tsx
+++ chapter3/ignews/src/pages/posts/[slug].tsx
@@ -39,7 +39,7 @@
     const session = await getSession({ req });
     const { slug } = params;
 
-    if (!session.activeSubscription) {
+    if (!session?.activeSubscription) {
       return {
         redirect: {
           destination: "/",

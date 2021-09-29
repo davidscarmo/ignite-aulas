1268d571ce7824427926b919206bdaaafee0051f
David Carmo <davidsc2812@gmail.com>
2021-09-29 05:27:57 -0300

    Criacao da interface de login

--- chapter4/dashgo/src/pages/index.tsx
+++ chapter4/dashgo/src/pages/index.tsx
@@ -1,6 +1,57 @@
-
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 export default function Home() {
   return (
-   <h2>Dashgo</h2>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="Email"> Email </FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password"> Senha </FormLabel>
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }

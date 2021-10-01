e7917075be2d4f19e7a97132b29c1f1cd1c9028b
David Carmo <davidsc2812@gmail.com>
2021-10-01 06:18:55 -0300

    Criacao da interface de listagem de usuarios

---
+++ chapter4/dashgo/src/pages/users/index.tsx
@@ -0,0 +1,82 @@
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Checkbox,
+  Text,
+} from "@chakra-ui/react";
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading>Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th w="6"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">David Carmo</Text>
+                    <Text fontWeight="sm" color="gray.300">
+                      davidsc2812@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>1 de Outubro, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}

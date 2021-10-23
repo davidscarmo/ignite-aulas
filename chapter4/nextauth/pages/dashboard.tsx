cd77f88ccd99e1a593ef2a6b760412ca53c48e0b
David Carmo <davidsc2812@gmail.com>
2021-10-23 14:22:15 -0300

    Criacao do hook de permissao

--- chapter4/nextauth/pages/dashboard.tsx
+++ chapter4/nextauth/pages/dashboard.tsx
@@ -1,29 +1,35 @@
-import { useContext, useEffect } from "react"
-import { AuthContext } from "../contexts/AuthContext"
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { UseCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
-import { withSSRAuth } from "../utils/withSSRAuth"
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
-
+  const { user } = useContext(AuthContext);
+  const userCanSeeMetrics = UseCan({
+    permissions: ["metrics.list"],
+  });
   useEffect(() => {
-    api.get('/me')
-      .then(response => console.log(response))
-  }, [])
+    api.get("/me").then((response) => console.log(response));
+  }, []);
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
-  )
+    <>
+      {" "}
+      <h1>Dashboard: {user?.email}</h1>{" "}
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
-  const response = await apiClient.get('/me');
+  const response = await apiClient.get("/me");
 
-  console.log(response.data)
+  console.log(response.data);
 
   return {
-    props: {}
-  }
-})
+    props: {},
+  };
+});

c1235d76bc853f793094f5548afac1bb4fb19bb9
David Carmo <davidsc2812@gmail.com>
2021-11-12 09:41:34 -0300

    Ex: test unitário com o active link

---
+++ chapter3/ignews/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,31 @@
+import { render } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+describe("ActiveLink Component", () => {
+  it("renders correctly", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClasseName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  it("adds active class if the link is currently active", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClasseName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+    expect(getByText("Home")).toHaveClass("active");
+  });
+});

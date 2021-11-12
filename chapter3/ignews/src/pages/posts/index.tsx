b55141873d797d69ab9fd2b602e525fcec7b1179
David Carmo <davidsc2812@gmail.com>
2021-11-12 15:36:17 -0300

    Testes da pagina posts

--- chapter3/ignews/src/pages/posts/index.tsx
+++ chapter3/ignews/src/pages/posts/index.tsx
@@ -25,8 +25,8 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <Link href={`/posts/${post.slug}`}>
-              <a key={post.slug}>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
                 <time>{post.updatedAt}</time>
                 <strong>{post.title}</strong>
                 <p>{post.abstract}</p>
